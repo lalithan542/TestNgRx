b89acab4b6191132f7172787b8a4405f3d5058b3
lalitha <lalithan622@gmail.com>
2021-05-13 14:17:11 -0400

    code commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+
+import { FitmentModule } from "../fitment/fitment.module";
+
+import { StoreModule, MetaReducer } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { VehicleEffects } from "../fitment/store/effects/vehicle.effects";
+import { reducers } from "../fitment/store/reducers";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FitmentModule,
+    StoreModule.forRoot(reducers),
+    HttpClientModule,
+    EffectsModule.forRoot([VehicleEffects])
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
